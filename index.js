0c0550d97e39c567a7e7933a1d68908f9c28dc89
tarasovyaroslav <tarasov.fortech@gmail.com>
2022-07-12 17:33:39 +0300

    complete main features & fix styles (need to fix some bugs)

--- index.js
+++ index.js
@@ -1,64 +1,199 @@
-const storage = window.localStorage;
+function getTodosString() {
+    return storage.getItem('todos');
+}
 
-const todoList = document.querySelector('.todo-list');
+function getTodosObject() {
+    const todoObj = JSON.parse(getTodosString());
+    return { ...todoObj };
+}
 
-const addForm = document.querySelector('.add-form');
+// function getTodoObjectByKey(key) {
+//     return getTodosObject()[key];
+// }
 
-const addInput = document.querySelector('#add-input');
+function setTodos(todos) {
+    storage.setItem('todos', JSON.stringify(todos));
+    return;
+}
 
-addForm.onsubmit = (e) => {
-    e.preventDefault();
-    const newTodo = {
-        text: addInput.value,
-        checked: false,
-    };
-    storage.setItem(Date.now(), JSON.stringify(newTodo));
-    addInput.value = '';
-};
+function switchTodo(elementName) {
+    const key = elementName.slice(1);
+    const todos = getTodosObject();
+    const todo = todos[key];
 
-document.addEventListener('click', (e) => {
-    // TODO: debug here
-
-    if (e.target.name && e.target.name[0] === 't') {
-        const key = e.target.name.slice(1);
-        const todo = JSON.parse(storage[key]);
-        if (todo.checked) {
-            todo.checked = false;
-        } else {
-            todo.checked = true;
-        }
-        storage.setItem(key, JSON.stringify(todo));
+    if (todo.checked) {
+        todo.checked = false;
+    } else {
+        todo.checked = true;
     }
-});
+    const checkbox = document.querySelector(`#${elementName}`);
+    checkbox.checked = todo.checked;
 
-function createTodo(id, todo) {
-    console.log(todo);
+    todos[key] = { ...todo };
+    setTodos(todos);
+    return;
+}
+
+function switchTodoByKeyboard(e) {
+    if (e.key === 'Enter' || e.key === ' ') {
+        switchTodo(document.activeElement.id);
+    }
+}
+
+function isTodosExists() {
+    const todos = getTodosString();
+
+    return todos !== null;
+}
+
+function isTodosEmpty() {
+    const todos = getTodosString();
+    return todos === '{}';
+}
+
+function initTodoStorage() {
+    storage.setItem('todos', '{}');
+    return;
+}
+
+function createTodoTemplate(id, todo) {
     return `
         <li class="todo-item">
             <label class="todo-item__content">
                 <input
                 class="todo-checked"
                 type="checkbox"
-                name="t${id}"
+                tabindex="0"
+                id="t${id}"
                 ${todo.checked ? 'checked' : ''}
                 />
                 <div class="checkmark"></div>
-                <span class="todo-text">${todo.text}</span>
-                <button>Delete</button>
+                <span class="todo-text">${todo.text}</span> 
+                <button class="remove-button btn" id="b${id}" tabindex="0">X</button>
             </label>
         </li>
     `;
 }
 
-// const todoContent = todos.reduce((content, todo) => {
-//     return content + createTodo(todo);
-// }, '');
+function addTodoToStorage() {
+    if (addInput.value.trim() === '') {
+        alert('Write some todo text!');
+        return;
+    }
 
-// todoList.innerHTML = todoContent;
+    if (!isTodosExists) {
+        initTodoStorage();
+    }
 
-for (let i = 0; i < storage.length; i++) {
-    let key = storage.key(i);
-    todoList.innerHTML += createTodo(key, JSON.parse(storage[key]));
+    // Prevent some code injection
+    const specialChars = {
+        '&': '&amp;',
+        '"': '&quot',
+        "'": '&#039;',
+        '<': '&lt;',
+        '>': '&gt;',
+    };
+    let newText = addInput.value;
+    Object.keys(specialChars).forEach((char) => {
+        newText = newText.replaceAll(char, specialChars[char]);
+    });
+
+    const id = Date.now();
+    const newTodoItem = {
+        [id]: {
+            text: newText,
+            checked: false,
+        },
+    };
+
+    const todos = getTodosObject();
+    const newTodos = Object.assign(todos, newTodoItem);
+
+    setTodos(newTodos);
+    renderTodos();
+    addInput.value = '';
+    return;
 }
 
-console.log(window.localStorage);
+function removeTodoFromStorage(e) {
+    const todos = getTodosObject();
+    const key = e.target.id.slice(1);
+    delete todos[key];
+    setTodos(todos);
+    renderTodos();
+    return;
+}
+
+function renderTodos() {
+    todoList.innerHTML = '';
+    if (isTodosExists && !isTodosEmpty()) {
+        const todos = getTodosObject();
+        // const parser = new DOMParser();
+        Object.keys(todos).forEach((key) => {
+            // document.createElement(
+            //     'li',
+            //     parser.parseFromString(
+            //         createTodoTemplate(key, todos[key]),
+            //         'text/html'
+            //     ).body
+            // );
+            todoList.innerHTML += createTodoTemplate(key, todos[key]);
+        });
+    } else {
+        initTodoStorage();
+        // todoList.insertBefore();
+        todoList.innerHTML =
+            '<div class="todo-content--empty">Create some todos!</div>';
+    }
+
+    // Add click-switch handlers on todo items
+    const todoItems = document.querySelectorAll('.todo-checked'); // todo items
+    if (isTodosExists() && !isTodosEmpty()) {
+        todoItems.forEach((todo) => {
+            todo.addEventListener('click', (e) =>
+                switchTodo(e.target.id)
+            );
+            todo.addEventListener('keypress', (e) => {
+                switchTodoByKeyboard(e);
+            });
+            todo.addEventListener('keyup', (e) => {
+                e.preventDefault();
+            });
+        });
+    }
+
+    // Add click-remove handlers on todo items
+    const removeButtons = document.querySelectorAll('.remove-button');
+    removeButtons.forEach((btn) => {
+        btn.addEventListener('click', removeTodoFromStorage);
+    });
+}
+
+/* Main */
+
+const storage = window.localStorage;
+
+const addForm = document.querySelector('.add-form'); // form to creating todo
+const addInput = document.querySelector('#add-input'); // text to add to todo item
+const todoList = document.querySelector('.todo-list'); // list where placing todo items
+
+// Add todo into local storage and rerender list
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodoToStorage();
+    renderTodos();
+});
+
+const clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener('click', (e) => {
+    const ok = confirm(
+        'Are you sure? It beyond retrieve remove all todos.'
+    );
+    if (ok) {
+        initTodoStorage();
+        renderTodos();
+    }
+});
+
+// Initial render
+renderTodos();
